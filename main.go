c0e1b51cac6bd4cc34ae134b3d9600607999e1e0
MishaYanov <misha.y1404@gmail.com>
2024-09-15 13:00:12 +0300

    connect db and add user func

--- main.go
+++ main.go
@@ -1,31 +1,75 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/MishaYanov/rssagg/internal/database"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("no port configured")
 	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("no DB configured")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("cannot connect to database")
+	}
+
+	queries := database.New(conn)
+
+	apiCfg := apiConfig{
+		DB: queries,
+	}
+
 	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthz", handleReadiness)
+	v1Router.Get("/err", handleError)
+
+	v1Router.Post("/users", apiCfg.HandleCreateUser)
+
+	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
 		Addr: ":" + portString,
 	}
 
-	log.Fatalf("Server starting on port %s", portString)
+	log.Printf("Server starting on port %s", portString)
 	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal("Error starting server")
